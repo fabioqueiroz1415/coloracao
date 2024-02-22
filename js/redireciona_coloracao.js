e0decbbb9a9aac0d92f337b0b7603867e320fe99
fabioqueiroz1415 <fabio_queiroz@discente.ufcat.edu.br>
2024-02-22 00:55:39 -0300

    alterações

--- js/redireciona_coloracao.js
+++ js/redireciona_coloracao.js
@@ -2,13 +2,13 @@ function escolherOpcao() {
     var opcao = document.getElementById('opcoes').value;
     switch(opcao) {
         case 'Coloração':
-            window.location.href = 'https://fabioqueiroz1415.github.io/coloracao/coloracao.html';
+            window.location.href = 'https://fabioqueiroz1415.github.io/coloracao/html/coloracao.html';
             break;
         case 'Harmônica':
-            window.location.href = 'https://fabioqueiroz1415.github.io/coloracao/harmonica.html';
+            window.location.href = 'https://fabioqueiroz1415.github.io/coloracao/html/harmonica.html';
             break;
         case 'Completa':
-            window.location.href = 'https://fabioqueiroz1415.github.io/coloracao/completa.html';
+            window.location.href = 'https://fabioqueiroz1415.github.io/coloracao/html/completa.html';
             break;
         default:
             console.error('Opção inválida!!');
